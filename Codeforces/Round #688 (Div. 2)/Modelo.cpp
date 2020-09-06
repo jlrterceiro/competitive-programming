bcbc218d3f0e721b8e69bc501314322cf9a784bf
José Luis Rodrigues Terceiro <jose.terceiro@tjpi.jus.br>
2020-09-06 04:07:17 -0300

    Ajeitando o nome das pastas

---
+++ Codeforces/Round #688 (Div. 2)/Modelo.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <limits>
+#include <iomanip>
+#include <bitset>
+
+#define INF 1000000000
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
