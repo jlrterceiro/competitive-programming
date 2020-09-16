c0172490302119c3cc08ba89f7b11ca4856426d6
José Luis Rodrigues Terceiro <jose.terceiro@tjpi.jus.br>
2020-09-16 01:22:17 -0300

    adicionando o resto

---
+++ Codeforces/Round #688 (Div. 2)/Modelo.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <limits>
+#include <iomanip>
+#include <bitset>
+
+
+#define INF 1000000000
+
+using namespace std;
+
+int main() {
+    printf("oi");
+    return 0;
+}
