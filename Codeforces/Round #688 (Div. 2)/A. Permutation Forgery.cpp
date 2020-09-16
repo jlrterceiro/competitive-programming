42af1b2d66a09020debe3ce7816fd41ea1b590b1
José Luis Rodrigues Terceiro <jose.terceiro@tjpi.jus.br>
2020-09-16 01:25:56 -0300

    adicionando o resto

---
+++ Codeforces/Round #688 (Div. 2)/A. Permutation Forgery.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <limits>
+#include <iomanip>
+#include <bitset>
+
+#define INF 1000000000
+
+using namespace std;
+
+#define MAXN 110
+
+int p[MAXN];
+int n;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &n);
+        int i;
+        for (i=1; i<=n; i++) {
+            scanf("%d", &p[i]);
+        }
+        printf("** ");
+        for (i=n; i>=1; i--) {
+            printf("%d ", p[i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
