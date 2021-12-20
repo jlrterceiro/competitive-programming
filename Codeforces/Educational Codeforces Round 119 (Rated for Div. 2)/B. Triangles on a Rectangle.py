7da463b64fa1f428abc65fda41f6c9af93ba2ff7
jlrterceiro <jlrterceiro@gmail.com>
2021-12-20 17:55:59 -0300

    Update B. Triangles on a Rectangle.py

--- Codeforces/Educational Codeforces Round 119 (Rated for Div. 2)/B. Triangles on a Rectangle.py
+++ Codeforces/Educational Codeforces Round 119 (Rated for Div. 2)/B. Triangles on a Rectangle.py
@@ -1,26 +1,22 @@
 
 t = int(input())
 while t>0:
-  
+
     w, h = map(int, input().split())
 
     ans = -1
     
     for i in range(2) :
             tokens = [ int(tok) for tok in input().split() ]
-            k = tokens[0]
-            x = tokens[1:]
+            k, x = tokens[0], tokens[1:]
             ans = max(ans, ((x[k-1]-x[0])*h))
 
     for i in range(2) :
             tokens = [ int(tok) for tok in input().split() ]
             k = tokens[0]
-            y = tokens[1:]
+            k, y = tokens[0], tokens[1:]
             ans = max(ans, ((y[k-1]-y[0])*w))
    
     print(ans)
 
     t-=1
-
-    
-    
