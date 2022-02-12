437279301c42af0b12d41e290155bf82cf7cbccd
jlrterceiro <jlrterceiro@gmail.com>
2022-02-12 15:49:01 -0300

    Create A - Pretty Permutations.cpp

---
+++ Codeforces/Round #728 (Div. 2)/A - Pretty Permutations.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 1000000000
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+        int i;
+        if (n%2==0) {
+            for (i=1; i<=n; i+=2) {
+                printf("%d %d ", i+1, i);
+            }
+        }
+        else {
+            printf("3 1 2 ");
+            for (i=3; i<=n; i+=2) {
+                printf("%d %d ", i+1, i);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
